1bf72580c8b9005be3f24f2e5896251ddf144df6
isi-frischmann <isabell.frischmann@gmail.com>
2018-06-04 20:49:00 -0700

    first upload

---
+++ FizzBuzzTest.java
@@ -0,0 +1,7 @@
+public class FizzBuzzTest {
+    public static void main(String[] args){
+        FizzBuzz k = new FizzBuzz();
+        String result = k.fizzBuzz(15);
+        System.out.println(result);
+    }
+}
